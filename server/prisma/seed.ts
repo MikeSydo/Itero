a2dc88d3c3122839fb01031b59137de5406e3b90
Mykhailo Sydoruk <mishasidoruk9@gmail.com>
2025-10-15 00:03:59 +0300

    Added controllers for board

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -4,18 +4,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function seed() {
-    const tasksList = await prisma.tasksList.create({
-        data: { name: 'Done' }
+    const board = await prisma.kanbanBoard.create({
+        data: { name: 'Board1' }
     });
+
+    const todoList = await prisma.tasksList.create({
+        data: { 
+            name: 'To-do',
+            board: { connect: { id: board.id } }
+        }
+    });
+    const doneList = await prisma.tasksList.create({
+        data: { 
+            name: 'Done',
+            board: { connect: { id: board.id } }
+        }
+    });
+
     await prisma.task.createMany({
         data: [
             {
                 name: 'Task 1',
-                listId: tasksList.id
+                listId: todoList.id
             }, 
             {
                 name: 'Task 2',
-                listId: tasksList.id
+                listId: todoList.id
+            },
+            {
+                name: 'Task 1',
+                listId: doneList.id
+            }, 
+            {
+                name: 'Task 2',
+                listId: doneList.id
             }
         ]
     });
